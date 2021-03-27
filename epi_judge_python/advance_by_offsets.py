cedf5ae1111a72f4165875197efc6108f27ee30b
Cameron Conn <cam@camconn.cc>
2021-03-27 06:08:42 -0500

    moar array problems

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -4,7 +4,26 @@ from test_framework import generic_test
 
 
 def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
+    i = 0
+    max_pos = 0
+    current = A[i]
+
+    # Edge case
+    if len(A) <= 1:
+        return True
+
+    while i < len(A) and i <= max_pos:
+        current = A[i]
+        target = i + current
+
+        max_pos = max(max_pos, target)
+
+        # If we are the farthest we can go, and have reached the end.
+        if max_pos == i and max_pos < len(A)-1:
+            return False
+
+        i += 1
+
     return True
 
 
--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -4,8 +4,29 @@ from test_framework import generic_test
 
 
 def multiply(num1: List[int], num2: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    sign = -1 if (num1[0] < 0) ^ (num2[0] < 0) else 1
+    num1[0] = abs(num1[0])
+    num2[0] = abs(num2[0])
+
+    output = [0]*(len(num1) + len(num2))
+
+    for i in reversed(range(len(num1))):
+        for j in reversed(range(len(num2))):
+            output[i + j + 1] += num1[i] * num2[j]
+            output[i + j] += output[i+j+1] // 10
+            output[i + j + 1] %= 10
+
+    # Remove extraneous leading zeros
+    while len(output) > 0 and output[0] == 0:
+        output.pop(0)
+
+    # If output number is empty, give back a 0
+    if len(output) == 0:
+        output.append(0)
+
+    output[0] *= sign
+
+    return output
 
 
 if __name__ == '__main__':
